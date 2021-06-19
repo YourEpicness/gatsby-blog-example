39c8266def06ed368e36a8ea302207db92b87add
Julaam Diop <jeebe.diop@gmail.com>
2021-06-19 14:46:41 -0400

    added example markdown pages, added slugs to node to get file path, and added packages to get markdown page info

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,20 @@
  * See: https://www.gatsbyjs.com/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  // console.log(node.internal.type)
+  const { createNodeField } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+    // called a slug: known as a file/navigation path to route to
+    const slug = createFilePath({ node, getNode })
+
+    //appending a new slug/file path
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
+  }
+}
