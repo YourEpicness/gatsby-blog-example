0b95778f07c7d990b9474504a082ec95eee35395
Julaam Diop <jeebe.diop@gmail.com>
2021-06-19 15:13:47 -0400

    added in styled components, and styled the links and created a template for blog posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,19 @@
 import * as React from "react"
 import { graphql, Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+
+const BlogLink = styled(Link)`
+  text-decoration: none;
+`
+
+const BlogTitle = styled.h3`
+  margin-bottom: 20px;
+  color: blue;
+`
 
 export default ({ data }) => {
   console.log(data)
@@ -15,9 +25,11 @@ export default ({ data }) => {
         <h4> {data.allMarkdownRemark.totalCount}</h4>
         {data.allMarkdownRemark.edges.map(({ node }) => (
           <div key={node.id}>
-            <span>
-              {node.frontmatter.title} - {node.frontmatter.date}
-            </span>
+            <BlogLink to={node.fields.slug}>
+              <BlogTitle>
+                {node.frontmatter.title} - {node.frontmatter.date}
+              </BlogTitle>
+            </BlogLink>
             <p>{node.excerpt} </p>
           </div>
         ))}
@@ -28,7 +40,7 @@ export default ({ data }) => {
 
 export const query = graphql`
   query {
-    allMarkdownRemark {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
       edges {
         node {
           id
@@ -37,6 +49,9 @@ export const query = graphql`
             title
             description
             date
+          }
+          fields {
+            slug
           }
           excerpt
         }
